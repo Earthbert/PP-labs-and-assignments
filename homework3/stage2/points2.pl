c93e6468d6d2629593422549b5c3f5648b526865
Earthbert <daraban_timotei@tutanota.com>
2023-05-17 09:25:15 +0300

    Separate homework3 into stages

---
+++ homework3/stage2/points2.pl
@@ -0,0 +1,15 @@
+
+vmpoints(Test, Points):-
+        member(Test:Points,
+               [
+                        % teste de la etapa 1
+                 at:0
+               , atL:0
+               , c2:0
+               , ccw:0
+               , rot:0
+               , match:0
+               , findrot:0
+
+                        % etapa 2
+                    ]).
